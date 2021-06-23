e4c5c79a55a57f794bbf1a8e156b922ddee776ca
Painezor <paine.exe@gmail.com>
2021-06-23 01:33:34 +0100

    Rename Pypeteer.py -> Browser.py Handle creation of browser instances within browser.py

--- ext/utils/browser.py
+++ ext/utils/browser.py
@@ -1,15 +1,53 @@
+"""Create and manipulate an instance of a headerless pyppeteer browser"""
 from io import BytesIO
 
+import pyppeteer
 from PIL import Image
 
 
+async def make_browser(bot):
+    """Spawn an instance of Pyppeteer"""
+    # # options = {"args": [
+    # #     '--no-sandbox',
+    # #     '--disable-setuid-sandbox',
+    # #     '--disable-infobars',
+    # #     '--single-process',
+    # #     '--no-zygote',
+    # #     '--no-first-run',
+    # #     '--window-position=0,0',
+    # #     '--ignore-certificate-errors',
+    # #     '--ignore-certificate-errors-skip-list',
+    # #     '--disable-dev-shm-usage',
+    # #     '--disable-accelerated-2d-canvas',
+    # #     '--disable-gpu',
+    # #     '--hide-scrollbars',
+    # #     '--disable-notifications',
+    # #     '--disable-background-timer-throttling',
+    # #     '--disable-backgrounding-occluded-windows',
+    # #     '--disable-breakpad',
+    # #     '--disable-component-extensions-with-background-pages',
+    # #     '--disable-extensions',
+    # #     '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+    # #     '--disable-ipc-flooding-protection',
+    # #     '--disable-renderer-backgrounding',
+    # #     '--enable-features=NetworkService,NetworkServiceInProcess',
+    # #     '--force-color-profile=srgb',
+    # #     '--metrics-recording-only',
+    # #     '--mute-audio'
+    # # ]}
+    #
+    # bot.browser = await pyppeteer.launch(options=options)
+    bot.browser = await pyppeteer.launch()
+
+
 async def fetch(page, url, xpath, clicks=None, deletes=None, screenshot=False, debug=False):
+    """Fetch a webpage's soruce code or an image"""
     deletes = [] if deletes is None else deletes
     clicks = [] if clicks is None else clicks
-    
+
     assert url.startswith("http"), f"{url} does not appear to be a valid url."
     await page.goto(url)  # DEBUG, old code: (url, {'waitUntil': 'networkidle0'})
-    
+
     src = await page.content()
     
     for x in deletes:
