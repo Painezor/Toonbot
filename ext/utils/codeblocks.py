036cb27147a18f06002b1e698dcc82264c38f40a
Painezor <Paine.exe@gmail.com>
2022-03-11 13:26:27 +0000

    Deprecate Codeblocks util.

--- ext/utils/codeblocks.py
+++
@@ -1,7 +0,0 @@
-"""Miscellaneous tools for creating codeblocks in discord"""
-import traceback
-
-def error_to_codeblock(error):
-    """Formatting of python errors into codeblocks"""
-    return f':no_entry_sign: {type(error).__name__}: {error}```py\n' \
-           f'{"".join(traceback.format_exception(type(error), error, error.__traceback__))}```'
