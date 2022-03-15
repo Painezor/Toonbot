a1b493d74227af1472e8931def60904942ee1ea8
Painezor <Paine.exe@gmail.com>
2022-03-15 13:32:52 +0000

    change dump_image file name for no apparent reason

--- ext/utils/image_utils.py
+++ ext/utils/image_utils.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 async def dump_image(bot: 'Client', img: BytesIO):
     """Dump an image to discord & return its URL to be used in embeds"""
     ch = bot.get_channel(874655045633843240)
-    img_msg = await ch.send(file=File(fp=img, filename="embed_image.png"))
+    img_msg = await ch.send(file=File(fp=img, filename="dumped_image.png"))
     url = img_msg.attachments[0].url
     return None if url == "none" else url
 
