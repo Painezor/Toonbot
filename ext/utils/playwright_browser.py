3fe28e4ee2b18830b2fea331ca6faf7680ac890f
Painezor <Paine.exe@gmail.com>
2023-02-22 16:26:51 +0000

    I'm in the process of fixing the linting. I also have ligma.

--- ext/utils/playwright_browser.py
+++ ext/utils/playwright_browser.py
@@ -1,18 +1,12 @@
 """Use Playwright to control a header-less Browser"""
 from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
 from playwright.async_api import async_playwright, Browser, ViewportSize
 
-if TYPE_CHECKING:
-	from core import Bot
-	from painezBot import PBot
 
-
-async def make_browser(bot: Bot | PBot) -> Browser:
-	"""Spawn an instance of Chromium to act as the header-less browser of the bot"""
-	browser = await (await async_playwright().start()).chromium.launch()
-	ctx = await browser.new_context(viewport=ViewportSize(height=1080, width=1920))
-	ctx.bot = bot
-	return ctx
+async def make_browser() -> Browser:
+    """Spawn an instance of Chromium to act as the headerless browser"""
+    pw = await async_playwright().start()
+    browser = await pw.chromium.launch(headless=True)
+    size = ViewportSize(height=1080, width=1920)
+    ctx = await browser.new_context(viewport=size)
+    return ctx
